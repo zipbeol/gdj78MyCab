efc6efa257ab6f359e94721b7aef8a3c955f9c81
seo_min_yeong <meano_@naver.com>
2024-06-20 12:33:48 +0900

    한 거 없는데 일단 커밋

--- src/main/java/com/my/cab/controller/FinanceController.java
+++ src/main/java/com/my/cab/controller/FinanceController.java
@@ -1,13 +1,41 @@
 package com.my.cab.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.my.cab.service.FinanceService;
 
 
 @Controller
 public class FinanceController { 
-	
-	Logger logger = LoggerFactory.getLogger(getClass());
 
+	Logger logger = LoggerFactory.getLogger(getClass());
+	@Autowired FinanceService financeService;
+
+	@RequestMapping(value="/finance/profit/list.go")
+	public ModelAndView financeProfit(HttpSession session) {
+		logger.info("재무관리 수익리스트");
+		ModelAndView mav = new ModelAndView("/finance/financeProfit");
+		// 세션 체크
+//		if(session.getAttribute("loginId")!=null) {
+//			mav.addObject("chk", "on");
+//		} else {
+//			mav.addObject("chk", "notOn");
+//		}
+		List<String> profitList = financeService.getProfitList();
+		 mav.addObject("profitList", profitList);
+		
+		return mav;
+	}
+	
 }
+
--- src/main/java/com/my/cab/service/FinanceService.java
+++ src/main/java/com/my/cab/service/FinanceService.java
@@ -1,5 +1,8 @@
 package com.my.cab.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -9,5 +12,13 @@ import org.springframework.stereotype.Service;
 public class FinanceService {
 	
 	Logger logger = LoggerFactory.getLogger(getClass());
+
+	public List<String> getProfitList() {
+        List<String> profitList = new ArrayList()<>();
+        profitList.add("");
+        profitList.add("");
+        profitList.add("");
+        return profitList;
+	}
 
 }

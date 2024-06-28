923d0d4455ae0fb306671f9275b31913c1f9280a
kim_dae_hyeon <kimdae6313@gmail.com>
2024-06-28 16:54:13 +0900

    DB table 수정 후 ApprovalDocDTO. ApprovalDTO, DocmentDTO 생성/수정, ApprovalController 기안서 작성 후  insert 마무리 작업 진행중 (60%)

--- src/main/java/com/my/cab/dao/ApprovalDAO.java
+++ src/main/java/com/my/cab/dao/ApprovalDAO.java
@@ -6,20 +6,28 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import com.my.cab.dto.ApprovalDTO;
 import com.my.cab.dto.ApprovalDocDTO;
 import com.my.cab.dto.ApprovalWriteFromDTO;
+import com.my.cab.dto.DocumentDTO;
 import com.my.cab.dto.EmpDTO;
 
 @Mapper
 public interface ApprovalDAO {
 
-	List<ApprovalWriteFromDTO> approvalWriteList();
+    List<ApprovalWriteFromDTO> approvalWriteList();
 
-	void save(ApprovalDocDTO htmlFileDto);
+    List<EmpDTO> getAllEmployees();
 
-	List<EmpDTO> getAllEmployees();
+    List<EmpDTO> searchEmployees(@Param("search") String search);
 
-	List<EmpDTO> searchEmployees(String search);
+    void saveAttachedFilePath(DocumentDTO docItemFileDTO);
+
+    void saveApprovalDoc(ApprovalDocDTO approvalDoc);
+
+    void saveDocument(DocumentDTO doc);
+
+    void saveApproval(ApprovalDTO approval);
 
 
 }
--- src/main/java/com/my/cab/dto/DocumentDTO.java
+++ src/main/java/com/my/cab/dto/DocumentDTO.java
@@ -1,5 +1,51 @@
 package com.my.cab.dto;
 
-public class DocumentDTO {
+import java.sql.Date;
 
+import org.apache.ibatis.type.Alias;
+
+@Alias(value="DocumentDTO")
+public class DocumentDTO {
+    private int appr_file_idx; // 첨부파일번호
+    private String appr_file_name; // 원본파일명
+    private String appr_file_new_name; // 저장파일명
+    private String appr_file_path; // 파일URL
+    private Date appr_file_dt; // 저장일
+	public int getAppr_file_idx() {
+		return appr_file_idx;
+	}
+	public void setAppr_file_idx(int appr_file_idx) {
+		this.appr_file_idx = appr_file_idx;
+	}
+	public String getAppr_file_name() {
+		return appr_file_name;
+	}
+	public void setAppr_file_name(String appr_file_name) {
+		this.appr_file_name = appr_file_name;
+	}
+	public String getAppr_file_new_name() {
+		return appr_file_new_name;
+	}
+	public void setAppr_file_new_name(String appr_file_new_name) {
+		this.appr_file_new_name = appr_file_new_name;
+	}
+	public String getAppr_file_path() {
+		return appr_file_path;
+	}
+	public void setAppr_file_path(String appr_file_path) {
+		this.appr_file_path = appr_file_path;
+	}
+	public Date getAppr_file_dt() {
+		return appr_file_dt;
+	}
+	public void setAppr_file_dt(Date appr_file_dt) {
+		this.appr_file_dt = appr_file_dt;
+	}
+	@Override
+	public String toString() {
+		return "DocumentDTO [appr_file_idx=" + appr_file_idx + ", appr_file_name=" + appr_file_name
+				+ ", appr_file_new_name=" + appr_file_new_name + ", appr_file_path=" + appr_file_path
+				+ ", appr_file_dt=" + appr_file_dt + "]";
+	}
+    
 }

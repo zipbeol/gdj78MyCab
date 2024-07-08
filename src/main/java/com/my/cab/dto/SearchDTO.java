4790adf94e9a5741377e00d253a182b063f5cd56
Jung_Su_Yeon <dkaneh7675@naver.com>
2024-07-08 17:09:44 +0900

    쏘리

--- src/main/java/com/my/cab/dto/SearchDTO.java
+++ src/main/java/com/my/cab/dto/SearchDTO.java
@@ -50,16 +50,301 @@ public class SearchDTO {
     private String dealCheckBox;
 
     private String dept_no;
-    private String emp_no;
-    
-    
-    
-    public String getEmp_no() {
+
+	public String getSearchText() {
+		return searchText;
+	}
+
+	public void setSearchText(String searchText) {
+		this.searchText = searchText;
+	}
+
+	public String getFilterTaxiModel() {
+		return filterTaxiModel;
+	}
+
+	public void setFilterTaxiModel(String filterTaxiModel) {
+		this.filterTaxiModel = filterTaxiModel;
+	}
+
+	public String getFilterIsActive() {
+		return filterIsActive;
+	}
+
+	public void setFilterIsActive(String filterIsActive) {
+		this.filterIsActive = filterIsActive;
+	}
+
+	public String getFilterStartDate() {
+		return filterStartDate;
+	}
+
+	public void setFilterStartDate(String filterStartDate) {
+		this.filterStartDate = filterStartDate;
+	}
+
+	public String getFilterEndDate() {
+		return filterEndDate;
+	}
+
+	public void setFilterEndDate(String filterEndDate) {
+		this.filterEndDate = filterEndDate;
+	}
+
+	public String getSortOrder() {
+		return sortOrder;
+	}
+
+	public void setSortOrder(String sortOrder) {
+		this.sortOrder = sortOrder;
+	}
+
+	public String getSortColumn() {
+		return sortColumn;
+	}
+
+	public void setSortColumn(String sortColumn) {
+		this.sortColumn = sortColumn;
+	}
+
+	public String getPro_filter() {
+		return pro_filter;
+	}
+
+	public void setPro_filter(String pro_filter) {
+		this.pro_filter = pro_filter;
+	}
+
+	public String getExp_filter() {
+		return exp_filter;
+	}
+
+	public void setExp_filter(String exp_filter) {
+		this.exp_filter = exp_filter;
+	}
+
+	public String getDeal_filter() {
+		return deal_filter;
+	}
+
+	public void setDeal_filter(String deal_filter) {
+		this.deal_filter = deal_filter;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+
+	public String getSearchIdx() {
+		return searchIdx;
+	}
+
+	public void setSearchIdx(String searchIdx) {
+		this.searchIdx = searchIdx;
+	}
+
+	public String getFilterIsDriverFault() {
+		return filterIsDriverFault;
+	}
+
+	public void setFilterIsDriverFault(String filterIsDriverFault) {
+		this.filterIsDriverFault = filterIsDriverFault;
+	}
+
+	public String getWhereCalled() {
+		return whereCalled;
+	}
+
+	public void setWhereCalled(String whereCalled) {
+		this.whereCalled = whereCalled;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public String getFilterIsRetired() {
+		return filterIsRetired;
+	}
+
+	public void setFilterIsRetired(String filterIsRetired) {
+		this.filterIsRetired = filterIsRetired;
+	}
+
+	public String getFilterForSearch() {
+		return filterForSearch;
+	}
+
+	public void setFilterForSearch(String filterForSearch) {
+		this.filterForSearch = filterForSearch;
+	}
+
+	public String getFilterAttResult() {
+		return filterAttResult;
+	}
+
+	public void setFilterAttResult(String filterAttResult) {
+		this.filterAttResult = filterAttResult;
+	}
+
+	public String getFilterAttDate() {
+		return filterAttDate;
+	}
+
+	public void setFilterAttDate(String filterAttDate) {
+		this.filterAttDate = filterAttDate;
+	}
+
+	public String getSearchEditText() {
+		return searchEditText;
+	}
+
+	public void setSearchEditText(String searchEditText) {
+		this.searchEditText = searchEditText;
+	}
+
+	public String getFilterEditDate() {
+		return filterEditDate;
+	}
+
+	public void setFilterEditDate(String filterEditDate) {
+		this.filterEditDate = filterEditDate;
+	}
+
+	public String getFilterEditResult() {
+		return filterEditResult;
+	}
+
+	public void setFilterEditResult(String filterEditResult) {
+		this.filterEditResult = filterEditResult;
+	}
+
+	public String getFilterforsearchEdit() {
+		return filterforsearchEdit;
+	}
+
+	public void setFilterforsearchEdit(String filterforsearchEdit) {
+		this.filterforsearchEdit = filterforsearchEdit;
+	}
+
+	public int getEmp_no() {
 		return emp_no;
 	}
 
-	public void setEmp_no(String emp_no) {
+	public void setEmp_no(int emp_no) {
 		this.emp_no = emp_no;
+	}
+
+	public String getEmp_name() {
+		return emp_name;
+	}
+
+	public void setEmp_name(String emp_name) {
+		this.emp_name = emp_name;
+	}
+
+	public String getNoticeWriter() {
+		return noticeWriter;
+	}
+
+	public void setNoticeWriter(String noticeWriter) {
+		this.noticeWriter = noticeWriter;
+	}
+
+	public String getNoticeTitle() {
+		return noticeTitle;
+	}
+
+	public void setNoticeTitle(String noticeTitle) {
+		this.noticeTitle = noticeTitle;
+	}
+
+	public String getSearchFilter() {
+		return searchFilter;
+	}
+
+	public void setSearchFilter(String searchFilter) {
+		this.searchFilter = searchFilter;
+	}
+
+	public String getSearchQuery() {
+		return searchQuery;
+	}
+
+	public void setSearchQuery(String searchQuery) {
+		this.searchQuery = searchQuery;
+	}
+
+	public String getFilterDriverName() {
+		return filterDriverName;
+	}
+
+	public void setFilterDriverName(String filterDriverName) {
+		this.filterDriverName = filterDriverName;
+	}
+
+	public String getFilterStartLocation() {
+		return filterStartLocation;
+	}
+
+	public void setFilterStartLocation(String filterStartLocation) {
+		this.filterStartLocation = filterStartLocation;
+	}
+
+	public String getFilterEndLocation() {
+		return filterEndLocation;
+	}
+
+	public void setFilterEndLocation(String filterEndLocation) {
+		this.filterEndLocation = filterEndLocation;
+	}
+
+	public String getFilterMinDistance() {
+		return filterMinDistance;
+	}
+
+	public void setFilterMinDistance(String filterMinDistance) {
+		this.filterMinDistance = filterMinDistance;
+	}
+
+	public String getFilterMaxDistance() {
+		return filterMaxDistance;
+	}
+
+	public void setFilterMaxDistance(String filterMaxDistance) {
+		this.filterMaxDistance = filterMaxDistance;
+	}
+
+	public String getFilterMinFare() {
+		return filterMinFare;
+	}
+
+	public void setFilterMinFare(String filterMinFare) {
+		this.filterMinFare = filterMinFare;
+	}
+
+	public String getFilterMaxFare() {
+		return filterMaxFare;
+	}
+
+	public void setFilterMaxFare(String filterMaxFare) {
+		this.filterMaxFare = filterMaxFare;
 	}
 
 	public String getFilterVacResult() {
@@ -78,316 +363,23 @@ public class SearchDTO {
 		this.filterVacDate = filterVacDate;
 	}
 
+	public String getDealCheckBox() {
+		return dealCheckBox;
+	}
+
+	public void setDealCheckBox(String dealCheckBox) {
+		this.dealCheckBox = dealCheckBox;
+	}
+
 	public String getDept_no() {
-        return dept_no;
-    }
-
-    public void setDept_no(String dept_no) {
-        this.dept_no = dept_no;
-    }
-
-    public String getFilterDriverName() {
-        return filterDriverName;
-    }
-
-    public void setFilterDriverName(String filterDriverName) {
-        this.filterDriverName = filterDriverName;
-    }
-
-    public String getFilterStartLocation() {
-        return filterStartLocation;
-    }
-
-    public void setFilterStartLocation(String filterStartLocation) {
-        this.filterStartLocation = filterStartLocation;
-    }
-
-    public String getFilterEndLocation() {
-        return filterEndLocation;
-    }
-
-    public void setFilterEndLocation(String filterEndLocation) {
-        this.filterEndLocation = filterEndLocation;
-    }
-
-    public String getFilterMinDistance() {
-        return filterMinDistance;
-    }
-
-    public void setFilterMinDistance(String filterMinDistance) {
-        this.filterMinDistance = filterMinDistance;
-    }
-
-    public String getFilterMaxDistance() {
-        return filterMaxDistance;
-    }
-
-    public void setFilterMaxDistance(String filterMaxDistance) {
-        this.filterMaxDistance = filterMaxDistance;
-    }
-
-    public String getFilterMinFare() {
-        return filterMinFare;
-    }
-
-    public void setFilterMinFare(String filterMinFare) {
-        this.filterMinFare = filterMinFare;
-    }
-
-    public String getFilterMaxFare() {
-        return filterMaxFare;
-    }
-
-    public void setFilterMaxFare(String filterMaxFare) {
-        this.filterMaxFare = filterMaxFare;
-    }
-
-    public String getSearchEditText() {
-        return searchEditText;
-    }
-
-    public void setSearchEditText(String searchEditText) {
-        this.searchEditText = searchEditText;
-    }
-
-    public String getFilterEditDate() {
-        return filterEditDate;
-    }
-
-    public void setFilterEditDate(String filterEditDate) {
-        this.filterEditDate = filterEditDate;
-    }
-
-    public String getFilterEditResult() {
-        return filterEditResult;
-    }
-
-    public void setFilterEditResult(String filterEditResult) {
-        this.filterEditResult = filterEditResult;
-    }
-
-    public String getFilterforsearchEdit() {
-        return filterforsearchEdit;
-    }
-
-    public void setFilterforsearchEdit(String filterforsearchEdit) {
-        this.filterforsearchEdit = filterforsearchEdit;
-    }
-
-    public String getFilterAttResult() {
-        return filterAttResult;
-    }
-
-    public void setFilterAttResult(String filterAttResult) {
-        this.filterAttResult = filterAttResult;
-    }
-
-    public String getFilterAttDate() {
-        return filterAttDate;
-    }
-
-    public void setFilterAttDate(String filterAttDate) {
-        this.filterAttDate = filterAttDate;
-    }
-
-    public String getPro_filter() {
-        return pro_filter;
-    }
-
-    public void setPro_filter(String pro_filter) {
-        this.pro_filter = pro_filter;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getSearchIdx() {
-        return searchIdx;
-    }
-
-    public void setSearchIdx(String searchIdx) {
-        this.searchIdx = searchIdx;
-    }
-
-    public String getFilterIsDriverFault() {
-        return filterIsDriverFault;
-    }
-
-    public void setFilterIsDriverFault(String filterIsDriverFault) {
-        this.filterIsDriverFault = filterIsDriverFault;
-    }
-
-    public String getWhereCalled() {
-        return whereCalled;
-    }
-
-    public void setWhereCalled(String whereCalled) {
-        this.whereCalled = whereCalled;
-    }
-
-    public String getFilterIsRetired() {
-        return filterIsRetired;
-    }
-
-    public void setFilterIsRetired(String filterIsRetired) {
-        this.filterIsRetired = filterIsRetired;
-    }
-
-    public String getSortOrder() {
-        return sortOrder;
-    }
-
-    public void setSortOrder(String sortOrder) {
-        this.sortOrder = sortOrder;
-    }
-
-    public String getSortColumn() {
-        return sortColumn;
-    }
-
-    public void setSortColumn(String sortColumn) {
-        this.sortColumn = sortColumn;
-    }
-
-    public int getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public String getFilterTaxiModel() {
-        return filterTaxiModel;
-    }
-
-    public void setFilterTaxiModel(String filterTaxiModel) {
-        this.filterTaxiModel = filterTaxiModel;
-    }
-
-    public String getFilterIsActive() {
-        return filterIsActive;
-    }
-
-    public void setFilterIsActive(String filterIsActive) {
-        this.filterIsActive = filterIsActive;
-    }
-
-    public String getFilterStartDate() {
-        return filterStartDate;
-    }
-
-    public void setFilterStartDate(String filterStartDate) {
-        this.filterStartDate = filterStartDate;
-    }
-
-    public String getFilterEndDate() {
-        return filterEndDate;
-    }
-
-    public void setFilterEndDate(String filterEndDate) {
-        this.filterEndDate = filterEndDate;
-    }
-
-    public String getSearchText() {
-        return searchText;
-    }
-
-    public void setSearchText(String searchText) {
-        this.searchText = searchText;
-    }
-
-    public String getExp_filter() {
-        return exp_filter;
-    }
-
-    public void setExp_filter(String exp_filter) {
-        this.exp_filter = exp_filter;
-    }
-
-    public String getFilterForSearch() {
-        return filterForSearch;
-    }
-
-    public void setFilterForSearch(String filterForSearch) {
-        this.filterForSearch = filterForSearch;
-    }
-
-    public String getDeal_filter() {
-        return deal_filter;
-    }
-
-    public void setDeal_filter(String deal_filter) {
-        this.deal_filter = deal_filter;
-    }
-
-    public String getNoticeWriter() {
-        return noticeWriter;
-    }
-
-    public void setNoticeWriter(String noticeWriter) {
-        this.noticeWriter = noticeWriter;
-    }
-
-    public String getNoticeTitle() {
-        return noticeTitle;
-    }
-
-    public void setNoticeTitle(String noticeTitle) {
-        this.noticeTitle = noticeTitle;
-    }
-
-    public String getSearchFilter() {
-        return searchFilter;
-    }
-
-    public void setSearchFilter(String searchFilter) {
-        this.searchFilter = searchFilter;
-    }
-
-    public String getSearchQuery() {
-        return searchQuery;
-    }
-
-    public void setSearchQuery(String searchQuery) {
-        this.searchQuery = searchQuery;
-    }
-
-    public String getDealCheckBox() {
-        return dealCheckBox;
-    }
-
-    public void setDealCheckBox(String dealCheckBox) {
-        this.dealCheckBox = dealCheckBox;
-    }
-
-	public int getEmp_no() {
-		return emp_no;
+		return dept_no;
 	}
 
-	public void setEmp_no(int emp_no) {
-		this.emp_no = emp_no;
+	public void setDept_no(String dept_no) {
+		this.dept_no = dept_no;
 	}
-
-	public String getEmp_name() {
-		return emp_name;
-	}
-
-	public void setEmp_name(String emp_name) {
-		this.emp_name = emp_name;
-	}
-
+    
+    
+    
+   
 }
